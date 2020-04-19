8786c6cfbfd69dfd5285af5956029f2a7d1df5b7
ElizKotuhova <ElizKotuhova@users.noreply.github.com>
2020-04-20 01:54:14 +0300

    Add New HomeTask

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+
+            int Result = 0;
+
+            a[0] = 1;
+            a[1] = 2;
+            a[2] = 3;
+            a[3] = 4;
+            a[4] = 5;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    Result += a[i];
+                }
+            }
+            Console.WriteLine(Result);
+            
+        }
+    }
+}
