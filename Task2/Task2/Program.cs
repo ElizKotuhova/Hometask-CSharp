0dfc8e3d68df44c10dca81adee085964f891b72a
ElizKotuhova <ElizKotuhova@users.noreply.github.com>
2020-04-20 22:55:39 +0300

    Add Task2

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length; i++)
+                if (i % 2 == 1)
+
+         Console.Write(str[i]);
+         
+
+        }
+    }
+}
